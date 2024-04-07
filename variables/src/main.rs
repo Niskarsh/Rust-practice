48a137f1c5c63e5599d4877b29e5beb31607f9d5
Niskarsh <niskarshk@gmail.com>
2024-04-07 12:49:20 +0530

    Practice for variables

---
+++ variables/src/main.rs
@@ -0,0 +1,22 @@
+
+// fn main(){
+//     let x1 = 40;
+//     let mut x2 = x1;
+//     x2 = x1-2;    // do not change this
+//     println!("x1 is: {} and x2 is: {}", x1,x2); // do not change this
+//  }
+
+
+// fn main() {
+//     let mut x1 = 40;
+//     let x2;
+//     x1 = x1 * 3;
+//     x2 = x1 - 2;
+//     println!("x1 is: {}, x2 is: {}", x1, x2);
+// }
+
+fn main() {
+    let a = "three";  // don't change this line
+    let a = 10; // don't change the namer of this variable
+    println!("a is: {}", a); 
+}
