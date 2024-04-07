a0f262035d65d359dc6027ac9d0ff0a2ab44dccc
Niskarsh <niskarshk@gmail.com>
2024-04-08 01:15:03 +0530

    Modules 2.1

--- modules/src/main.rs
+++ modules/src/main.rs
@@ -47,30 +47,61 @@
 
 // Problem #3: Re-export the items properly so that the code compiles
 
-mod graphics {
+// mod graphics {
 
-    // Re-export the 'show_area' function for easier access
-    // Re-export the 'calculate_area' function for easier access
-    pub use shapes::calculate_area;
-    pub use display::show_area;
+//     // Re-export the 'show_area' function for easier access
+//     // Re-export the 'calculate_area' function for easier access
+//     pub use shapes::calculate_area;
+//     pub use display::show_area;
 
-    pub mod shapes {
-        pub fn calculate_area(radius: f64) -> f64 {
-            std::f64::consts::PI * radius * radius
-        }
+//     pub mod shapes {
+//         pub fn calculate_area(radius: f64) -> f64 {
+//             std::f64::consts::PI * radius * radius
+//         }
+//     }
+//     pub mod display {
+//         pub fn show_area(shape: &str, area: f64) {
+//             println!("The area of the {} is: {}", shape, area);
+//         }
+//     }
+// }
+
+// use graphics::calculate_area; // fix this line
+// use graphics::show_area; // fix this line
+// fn main() {
+//     let radius = 3.0;
+//     let area = calculate_area(radius);
+
+//     show_area("circle", area);
+// }
+
+
+// Problem 1: Complete the code by bringing the required items into scope.
+
+
+mod seasons {
+
+    pub enum Season {
+        Spring,
+        Summer,
+        Autumn,
+        Winter,
     }
-    pub mod display {
-        pub fn show_area(shape: &str, area: f64) {
-            println!("The area of the {} is: {}", shape, area);
+
+    pub fn is_holiday(season: &Season) -> bool {
+        match season {
+            Season::Summer => true,
+            _ => false,
         }
     }
 }
 
-use graphics::calculate_area; // fix this line
-use graphics::show_area; // fix this line
 fn main() {
-    let radius = 3.0;
-    let area = calculate_area(radius);
-
-    show_area("circle", area);
+    let current_season = seasons::Season::Autumn;
+    if seasons::is_holiday(&current_season) {
+        println!("It's a holiday season! Time for a vacation!");
+    } else {
+        println!("Regular work season. Keep hustling!");
+    }
 }
+
