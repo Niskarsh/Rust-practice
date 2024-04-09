f9a9dd39c4dd42abd7b39631c1f1a31e17258df3
Niskarsh <niskarshk@gmail.com>
2024-04-09 09:52:58 +0530

    generics 1.2

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -6,22 +6,47 @@
 Define enum here
 */
 
-enum Operation<T> {
-    Addition(T, T),
-    Multiplication(T, T),
-    Subtraction(T, T),
-    Division(T, T),
+// enum Operation<T> {
+//     Addition(T, T),
+//     Multiplication(T, T),
+//     Subtraction(T, T),
+//     Division(T, T),
+// }
+
+// // impl<T> Operation<T> {
+// //     fn Addition (m: T, n: T) -> T {
+// //         m+n
+// //     }
+// //  }
+
+// fn main() {
+//     let op_1 = Operation::Addition(5, 10);
+//     let op_2 = Operation::Multiplication(3.5, 2.0);
+//     let op_3 = Operation::Subtraction(3.5, 2.0);
+//     let op_4 = Operation::Division(2, 3);
+// }
+
+// Problem 2: Fix the code below so that it compiles 
+
+
+struct Container<T> {
+    value: T,
 }
 
-// impl<T> Operation<T> {
-//     fn Addition (m: T, n: T) -> T {
-//         m+n
-//     }
-//  }
-
-fn main() {
-    let op_1 = Operation::Addition(5, 10);
-    let op_2 = Operation::Multiplication(3.5, 2.0);
-    let op_3 = Operation::Subtraction(3.5, 2.0);
-    let op_4 = Operation::Division(2, 3);
+impl<T> Container<T> {
+    fn create(value: T) -> Container<T> {       // something wrong here 
+        Container { value }
+    }
 }
+
+impl Container<i32> {
+    fn display(&self) {
+        println!("The value inside the container is: {}", self.value);
+    }
+
+    // fn create(value: i32) -> Container<i32> {
+    //     Container { value }
+    // }
+}
+
+fn main(){}
