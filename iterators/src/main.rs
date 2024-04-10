9b6015b855197a31d66e8ede4732889d6c021a71
Niskarsh <niskarshk@gmail.com>
2024-04-10 18:09:41 +0530

    Itertors 1.2

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,36 +1,70 @@
 // Problem 1: Compile the code by adding the definition for the next method
 
-struct Counter {
-    current: u32,
-    max: u32,
-}
+// struct Counter {
+//     current: u32,
+//     max: u32,
+// }
 
-impl Counter {
-    fn new(max: u32) -> Counter {
-        Counter { current: 0, max }
-    }
-}
+// impl Counter {
+//     fn new(max: u32) -> Counter {
+//         Counter { current: 0, max }
+//     }
+// }
 
-impl Iterator for Counter {
-    type Item = u32;
+// impl Iterator for Counter {
+//     type Item = u32;
 
-    fn next(&mut self) -> Option<Self::Item> { 
-        /* Add code here */
-        let c = self.current.clone();
-        if c < self.max {
-            self.current = self.current + 1;
-            Some(c)
-        } else {
-            None
-        }
+//     fn next(&mut self) -> Option<Self::Item> { 
+//         /* Add code here */
+//         let c = self.current.clone();
+//         if c < self.max {
+//             self.current = self.current + 1;
+//             Some(c)
+//         } else {
+//             None
+//         }
          
+//     }
+// }
+
+// fn main() {
+//     let mut counter = Counter::new(3);
+//     assert!(matches!(counter.next(), Some(0)));
+//     assert!(matches!(counter.next(), Some(1)));
+//     assert!(matches!(counter.next(), Some(2)));
+//     assert!(matches!(counter.next(), None));
+// }
+
+// Problem 2: Complete the into_iter function definition 
+
+struct Person {
+    name: String,
+    age: u32,
+    occupation: String,
+}
+
+impl IntoIterator for Person {
+    type Item = String;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        /* Your code here 
+        Hint: Should return a vector of Strings, 
+        representing the fields of the struct  */
+        vec![self.name, self.occupation].into_iter()
     }
 }
 
 fn main() {
-    let mut counter = Counter::new(3);
-    assert!(matches!(counter.next(), Some(0)));
-    assert!(matches!(counter.next(), Some(1)));
-    assert!(matches!(counter.next(), Some(2)));
-    assert!(matches!(counter.next(), None));
+    let person = Person {
+        name: "Alice".to_string(),
+        age: 30,
+        occupation: "Software Engineer".to_string(),
+    };
+
+    let mut person_iterator = person.into_iter();
+
+    while let Some(property) = person_iterator.next() {
+        println!("{}", property);
+    }
 }
