8c1a7944c3fa13176110f9d3864e961c7890fe6c
Niskarsh <niskarshk@gmail.com>
2024-04-10 18:04:04 +0530

    Itertors 1.1

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,3 +1,36 @@
+// Problem 1: Compile the code by adding the definition for the next method
+
+struct Counter {
+    current: u32,
+    max: u32,
+}
+
+impl Counter {
+    fn new(max: u32) -> Counter {
+        Counter { current: 0, max }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> { 
+        /* Add code here */
+        let c = self.current.clone();
+        if c < self.max {
+            self.current = self.current + 1;
+            Some(c)
+        } else {
+            None
+        }
+         
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut counter = Counter::new(3);
+    assert!(matches!(counter.next(), Some(0)));
+    assert!(matches!(counter.next(), Some(1)));
+    assert!(matches!(counter.next(), Some(2)));
+    assert!(matches!(counter.next(), None));
 }
