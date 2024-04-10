1d7f69d08c73ad3ddbbe47fe0f7dbc7870050527
Niskarsh <niskarshk@gmail.com>
2024-04-10 11:55:33 +0530

    Trait objects 1.2

--- traitObjects/src/main.rs
+++ traitObjects/src/main.rs
@@ -1,59 +1,115 @@
 // Problem 1: fix the 'describe_animal' function signature and also fix the calls to it in main
 
-trait Animal {
-    fn name(&self) -> &str;
-    fn make_sound(&self);
-    // fn describe_animal(&self) { // This line needs a fix
-    //     println!("The {} says:", self.name());
-    //     self.make_sound();
-    // }
+// trait Animal {
+//     fn name(&self) -> &str;
+//     fn make_sound(&self);
+//     // fn describe_animal(&self) { // This line needs a fix
+//     //     println!("The {} says:", self.name());
+//     //     self.make_sound();
+//     // }
+// }
+
+// struct Lion {
+//     name: String,
+// }
+
+// impl Animal for Lion {
+//     fn name(&self) -> &str {
+//         &self.name
+//     }
+
+//     fn make_sound(&self) {
+//         println!("Roar!");
+//     }
+// }
+
+// struct Penguin {
+//     name: String,
+// }
+
+// impl Animal for Penguin {
+//     fn name(&self) -> &str {
+//         &self.name
+//     }
+
+//     fn make_sound(&self) {
+//         println!("Honk!");
+//     }
+// }
+
+// fn describe_animal(animal: &dyn Animal) {
+//     println!("The {} says:", animal.name());
+//     animal.make_sound();
+// }
+
+
+// fn main() {
+//     let lion = Lion {
+//         name: "Simba".to_string(),
+//     };
+//     let penguin = Penguin {
+//         name: "Happy Feet".to_string(),
+//     };
+
+//     // The calls to function needs fixes
+//     // lion.describe_animal();
+//     // penguin.describe_animal();
+//     describe_animal(&lion);
+//     describe_animal(&penguin);
+// }
+
+
+// Problem 2: Fix the code by making suitable changes to the signatures 
+// of the functions 'get_vehicle' and 'operate_vehicle' 
+
+trait Vehicle {
+    fn start_engine(&self) -> String;
+    fn drive(&self) -> String;
 }
 
-struct Lion {
-    name: String,
-}
+struct Car;
 
-impl Animal for Lion {
-    fn name(&self) -> &str {
-        &self.name
+struct Bicycle;
+
+impl Vehicle for Car {
+    fn start_engine(&self) -> String {
+        "🚗 Engine started".to_string()
     }
 
-    fn make_sound(&self) {
-        println!("Roar!");
+    fn drive(&self) -> String {
+        "🚗 Driving the car".to_string()
     }
 }
 
-struct Penguin {
-    name: String,
-}
-
-impl Animal for Penguin {
-    fn name(&self) -> &str {
-        &self.name
+impl Vehicle for Bicycle {
+    fn start_engine(&self) -> String {
+        "🚲 No engine to start for a bicycle".to_string()
     }
 
-    fn make_sound(&self) {
-        println!("Honk!");
+    fn drive(&self) -> String {
+        "🚲 Pedaling the bicycle".to_string()
     }
 }
 
-fn describe_animal(animal: &dyn Animal) {
-    println!("The {} says:", animal.name());
-    animal.make_sound();
+fn get_vehicle(vehicle_type: &str) -> Box<dyn Vehicle> { // This line needs a fix 
+    match vehicle_type {
+        "car" => Box::new(Car),
+        "bicycle" => Box::new(Bicycle),
+        _ => panic!("No vehicle of that type available"),
+    }
 }
 
+fn operate_vehicle(driver: &dyn Vehicle) { // This line needs a fix 
+    println!("{}", driver.start_engine());
+    println!("{}", driver.drive());
+}
 
 fn main() {
-    let lion = Lion {
-        name: "Simba".to_string(),
-    };
-    let penguin = Penguin {
-        name: "Happy Feet".to_string(),
-    };
+    // Do not change code in main
+    let my_vehicle = get_vehicle("car"); 
+    operate_vehicle(&*my_vehicle);
 
-    // The calls to function needs fixes
-    // lion.describe_animal();
-    // penguin.describe_animal();
-    describe_animal(&lion);
-    describe_animal(&penguin);
+    let my_vehicle = get_vehicle("bicycle");
+    operate_vehicle(my_vehicle.as_ref());
 }
+
