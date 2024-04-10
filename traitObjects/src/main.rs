e902a1a28c3fa4e71b6027a52cf9c220b3121192
Niskarsh <niskarshk@gmail.com>
2024-04-10 12:12:47 +0530

    Trait objects 1.3

--- traitObjects/src/main.rs
+++ traitObjects/src/main.rs
@@ -62,6 +62,60 @@
 // Problem 2: Fix the code by making suitable changes to the signatures 
 // of the functions 'get_vehicle' and 'operate_vehicle' 
 
+// trait Vehicle {
+//     fn start_engine(&self) -> String;
+//     fn drive(&self) -> String;
+// }
+
+// struct Car;
+
+// struct Bicycle;
+
+// impl Vehicle for Car {
+//     fn start_engine(&self) -> String {
+//         "🚗 Engine started".to_string()
+//     }
+
+//     fn drive(&self) -> String {
+//         "🚗 Driving the car".to_string()
+//     }
+// }
+
+// impl Vehicle for Bicycle {
+//     fn start_engine(&self) -> String {
+//         "🚲 No engine to start for a bicycle".to_string()
+//     }
+
+//     fn drive(&self) -> String {
+//         "🚲 Pedaling the bicycle".to_string()
+//     }
+// }
+
+// fn get_vehicle(vehicle_type: &str) -> Box<dyn Vehicle> { // This line needs a fix 
+//     match vehicle_type {
+//         "car" => Box::new(Car),
+//         "bicycle" => Box::new(Bicycle),
+//         _ => panic!("No vehicle of that type available"),
+//     }
+// }
+
+// fn operate_vehicle(driver: &dyn Vehicle) { // This line needs a fix 
+//     println!("{}", driver.start_engine());
+//     println!("{}", driver.drive());
+// }
+
+// fn main() {
+//     // Do not change code in main
+//     let my_vehicle = get_vehicle("car"); 
+//     operate_vehicle(&*my_vehicle);
+
+//     let my_vehicle = get_vehicle("bicycle");
+//     operate_vehicle(my_vehicle.as_ref());
+// }
+
+
+// Problem 3: Fix the code by adding a proper type annotation for the vector in main
+
 trait Vehicle {
     fn start_engine(&self) -> String;
     fn drive(&self) -> String;
@@ -91,7 +145,7 @@ impl Vehicle for Bicycle {
     }
 }
 
-fn get_vehicle(vehicle_type: &str) -> Box<dyn Vehicle> { // This line needs a fix 
+fn get_vehicle(vehicle_type: &str) -> Box<dyn Vehicle> {
     match vehicle_type {
         "car" => Box::new(Car),
         "bicycle" => Box::new(Bicycle),
@@ -99,17 +153,21 @@ fn get_vehicle(vehicle_type: &str) -> Box<dyn Vehicle> { // This line needs a fi
     }
 }
 
-fn operate_vehicle(driver: &dyn Vehicle) { // This line needs a fix 
+fn operate_vehicle(driver: &dyn Vehicle) {
     println!("{}", driver.start_engine());
     println!("{}", driver.drive());
 }
 
 fn main() {
-    // Do not change code in main
-    let my_vehicle = get_vehicle("car"); 
-    operate_vehicle(&*my_vehicle);
+    let vehicle_1 = get_vehicle("car");
+    let vehicle_2 = get_vehicle("car");
+    let vehicle_3 = get_vehicle("bicycle");
 
-    let my_vehicle = get_vehicle("bicycle");
-    operate_vehicle(my_vehicle.as_ref());
+    let vehicles = vec![vehicle_1.as_ref(), vehicle_2.as_ref(), vehicle_3.as_ref()]; // Fix this line
+
+    for v in vehicles {
+        operate_vehicle(v);
+    }
 }
+
 
