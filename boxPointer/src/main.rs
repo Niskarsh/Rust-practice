466125815b5db145e9c44b83abe2e9c7f19d4dca
Niskarsh <niskarshk@gmail.com>
2024-04-11 14:34:32 +0530

    box pointer 1.2

--- boxPointer/src/main.rs
+++ boxPointer/src/main.rs
@@ -1,8 +1,26 @@
-// Problem 1: Fix the code below so that it compiles 
+// // Problem 1: Fix the code below so that it compiles 
 
-enum BinaryTree {
-    Leaf,
-    Node(i32, Box<BinaryTree>, Box<BinaryTree>),
+// enum BinaryTree {
+//     Leaf,
+//     Node(i32, Box<BinaryTree>, Box<BinaryTree>),
+// }
+
+// fn main() {}
+
+// Problem 2: Fix the code by completing the function signature
+
+struct Wrapper {
+    data: String,
 }
 
-fn main() {}
+fn modify_data(mut wrapper: Box<Wrapper>) -> Box<Wrapper> {
+    wrapper.data = String::from("Modified");
+    wrapper
+}
+
+fn main() {
+    let original_wrapper = Box::new(Wrapper {
+        data: String::from("Original"),
+    });
+    let modified_wrapper = modify_data(original_wrapper);
+}
