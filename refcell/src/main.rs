171a975beed497da716c08e54efcafee6a736572
Niskarsh <niskarshk@gmail.com>
2024-04-11 23:52:31 +0530

    refcell pointer 1.1

--- refcell/src/main.rs
+++ refcell/src/main.rs
@@ -1,3 +1,17 @@
+// Problem 1: fill in the code for TODO's
+
+use std::cell::RefCell;
 fn main() {
-    println!("Hello, world!");
+    let data: RefCell<Option<i32>> = RefCell::new(Some(42));
+
+    // TODO: Use borrow_mut to safely modify the value inside the RefCell to None.
+    let mut a = data.borrow_mut();
+    *a = Some(34);
+    drop(a);
+
+    if *(data.borrow()) != None {
+        println!("Final value: {:?}", data.borrow());
+    } else {
+        println!("No value present.");
+    }
 }
