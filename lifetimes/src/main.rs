462116863833050804d4ea05330dd1ed19a58ffe
Niskarsh <niskarshk@gmail.com>
2024-04-11 13:22:20 +0530

    Lifetimes 1.5

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -57,16 +57,53 @@
 
 // Problem 1: Add lifetime annotations in the function signature
 
-fn some_if_greater<'a, 'b>(number: &'a i32, greater_than: &'b i32) -> Option<&'a i32> {
-    if number > greater_than {
-        Some(number)
-    } else {
-        None
+// fn some_if_greater<'a, 'b>(number: &'a i32, greater_than: &'b i32) -> Option<&'a i32> {
+//     if number > greater_than {
+//         Some(number)
+//     } else {
+//         None
+//     }
+// }
+// fn main() {
+//     let num_1 = 7;
+//     let greater_val = 4;
+//     let test = some_if_greater(&num_1, &greater_val);
+//     println!("{:?}", test);
+// }
+
+// Problem 1: The code is compiling. However, I want you to remove the 
+// extra lifetimes parameters from function signatures.  
+
+struct DataHolder<'a> {
+    data: Vec<&'a str>,
+}
+
+impl<'a> DataHolder<'a> {
+    fn initialize() -> Self {
+        DataHolder { data: Vec::new() }
+    }
+
+    fn add_data(&mut self, item: &'a str) {
+        self.data.push(item);
+    }
+
+    fn extract_data_containing_substring(&mut self, sub: & str) -> & str {
+        for i in 0..self.data.len() {
+            if self.data[i].contains(sub) {
+                return self.data.remove(i);
+            }
+        }
+        panic!("Data containing substring not found");
     }
 }
+
 fn main() {
-    let num_1 = 7;
-    let greater_val = 4;
-    let test = some_if_greater(&num_1, &greater_val);
-    println!("{:?}", test);
+    let mut my_data = DataHolder::initialize();
+    my_data.add_data("Apple");
+    my_data.add_data("Banana");
+    my_data.add_data("Cherry");
+    my_data.add_data("Date");
+    let extracted = my_data.extract_data_containing_substring("na");
+    println!("Extracted: {}", extracted);
+    assert_eq!(my_data.data.len(), 3);
 }
