87a5dbff55518e12d951ec2fca18db137e5cbfb9
Niskarsh <niskarshk@gmail.com>
2024-04-11 12:26:46 +0530

    Lifetimes 1.1

---
+++ lifetimes/src/main.rs
@@ -0,0 +1,11 @@
+// Problem 1: Fix the code by moving the indicated line to approperiate location
+
+fn main() {
+    let mut some_str = String::from("I am String");
+    let ref1 = &some_str;
+    println!("{ref1}"); // move this line only
+    let ref2 = &mut some_str;
+    ref2.push_str(" additional information");   
+    println!("{ref2}");
+}
+
